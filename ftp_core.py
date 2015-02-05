6078c8b22084a9704d3226a005f00bda6bbbdb28
Сашко <penguinalex@yandex.ru>
2015-02-06 01:56:05 +0500

    Установка соединения, обработка кода ответа и сообщения, команда noop

--- ftp_core.py
+++ ftp_core.py
@@ -1,26 +1,49 @@
-__author__ = 'alexandra'
+__author__ = "alexandra"
+
+
 import socket
+import chardet
+import sys
 
 
 class CommandConnection:
     def __init__(self, serv_address, port=21):
         self.serv_address = serv_address
         self.port = port
+        self.encoding = "UTF-8"
+        self.connection_established = False
         self.command_socket = socket.socket()
 
     def make_connection(self):
-        self.command_socket.connect((self.serv_address, self.port))
-        self.reply = self.command_socket.recv(1024)
+        try:
+            self.command_socket.connect((self.serv_address, self.port))
+        except socket.error as sock_err:
+            print(sock_err, file=sys.stderr)
+            quit("Извините, не удаётся установить соединение")
+        code, message = self.get_reply()
+        if code == 220:
+            print(message)
+            return True
+        else:
+            print(code, message)
+            quit("Извините, не удаётся установить соединение")
+            return False
 
-    def parse_reply(self):
-        return (self.reply[:3], self.reply[4:])
+    def close_connection(self):
+        self.command_socket.close()
+
+    def parse_reply(self, reply):
+        return reply[:3], reply[4:]
 
     def send_cmd(self, cmd):
-        pass
+        self.command_socket.sendall((cmd + "\r\n").encode(self.encoding))
 
     def get_reply(self):
-        pass
-
+        reply = self.command_socket.recv(8196)
+        code, message = self.parse_reply(reply)
+        self.encoding = chardet.detect(message)["encoding"]  # detect encoding
+        code, message = int(code.decode(self.encoding)), message.decode(self.encoding)
+        return code, message
 
 
 class DataConnection:
@@ -41,17 +64,45 @@ class DataConnection:
 
 
 class Commands:
-    def __init__(self):
+    """
+    Все команды описаны в документе RFC 959 в главе 4.
+    URL: https://tools.ietf.org/html/rfc959
+    """
+    @staticmethod
+    def login(command_connection, user, passwd):
+
+        """
+        USER NAME (USER)
+        PASSWORD (PASS)
+        :param user:
+        :param passwd:
+        """
         pass
 
-    def login(self, user, passwd):
+    @staticmethod
+    def cd(command_connection):
+        """
+        CWD
+        :rtype : object
+        """
         pass
 
-    def logout(self):
+    @staticmethod
+    def logout(command_connection):
         pass
 
-    def pwd(self):
+    @staticmethod
+    def pwd(command_connection):
         pass
 
-    def noop(self):
-        pass
+    @staticmethod
+    def noop(command_connection):
+        """
+        NOOP (NOOP)
+        Выполнение данной команды ни на что ее влияет.
+        Единственное полезное свойство - проверка/поддержание соединения.
+        Возвращает "OK!"
+        """
+        command_connection.send_cmd("NOOP")
+        code, message = command_connection.get_reply()
+        print(code, message)
--- tests.py
+++ tests.py
@@ -1,11 +1,24 @@
 __author__ = 'alexandra'
 import unittest
 from ftp_core import CommandConnection
+from ftp_core import Commands
 
-class Test01_make_connection(unittest.TestCase):
+
+class FTPTest(unittest.TestCase):
+    """
+        Функция возвращает True в случае удачного выполнения иначе False
+    """
+    comm_connection = None
+
     def test01_establish_connection(self):
-        comm_connection = CommandConnection("88.85.196.86")
-        comm_connection.make_connection()
+        print("===Установка соединения===")
+        FTPTest.comm_connection = CommandConnection("88.85.196.86")
+        result = FTPTest.comm_connection.make_connection()
+        self.assertTrue(result)
+
+    def test02_noop(self):
+        print("===NOOP===")
+        Commands.noop(FTPTest.comm_connection)
 
 
 
