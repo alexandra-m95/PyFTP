b49b3ab778685d39005131ebdcc0b6bfad73aad2
Сашко <penguinalex@yandex.ru>
2015-02-07 17:28:50 +0500

    Реализованы фунции pwd, cwd, login(user, pass)

--- ftp_core.py
+++ ftp_core.py
@@ -1,9 +1,9 @@
 __author__ = "alexandra"
 
-
 import socket
 import chardet
 import sys
+import getpass
 
 
 class CommandConnection:
@@ -16,17 +16,24 @@ class CommandConnection:
 
     def make_connection(self):
         try:
+            self.command_socket.settimeout(5)
             self.command_socket.connect((self.serv_address, self.port))
-        except socket.error as sock_err:
-            print(sock_err, file=sys.stderr)
-            quit("Извините, не удаётся установить соединение")
+        except (socket.error, OSError) as err:
+            print("Извините, не удаётся установить соединение.")
+            # твоя задача - вывести минимальный трейсбек с номером ошибки и сообщением
+            # в виде
+            #    Ошибка: Connection timed out. Код ответа: 110.
+            #    Ошибка: Transport endpoint is not connected. Код ответа: 107.
+            # у сокета есть параметр - время ожидания. Его нужно задать небольшим чтобы не ждать ответов сервера по пол минуты
+            print(err)
+            # Сокет как и файл в случае ошибок нужно закрывать.
+            return False
         code, message = self.get_reply()
         if code == 220:
             print(message)
             return True
         else:
-            print(code, message)
-            quit("Извините, не удаётся установить соединение")
+            print("Извините, не удаётся установить соединение")
             return False
 
     def close_connection(self):
@@ -42,7 +49,7 @@ class CommandConnection:
         reply = self.command_socket.recv(8196)
         code, message = self.parse_reply(reply)
         self.encoding = chardet.detect(message)["encoding"]  # detect encoding
-        code, message = int(code.decode(self.encoding)), message.decode(self.encoding)
+        code, message = int(code.decode(self.encoding)), message.decode(self.encoding).strip()
         return code, message
 
 
@@ -68,32 +75,56 @@ class Commands:
     Все команды описаны в документе RFC 959 в главе 4.
     URL: https://tools.ietf.org/html/rfc959
     """
-    @staticmethod
-    def login(command_connection, user, passwd):
 
+    @staticmethod
+    def login(command_connection, username="anonymous", password=""):
         """
         USER NAME (USER)
         PASSWORD (PASS)
-        :param user:
-        :param passwd:
         """
-        pass
+        def user():
+            nonlocal username
+            # username = input("Пожалуйста введите имя пользователя: ")
+            command_connection.send_cmd("USER " + username)
+            return command_connection.get_reply()
+
+        def passwd():
+            nonlocal password
+            # password = getpass.getpass("Пожалуйста введите пароль: ")
+            command_connection.send_cmd("PASS " + password)
+            return command_connection.get_reply()
+
+        u_code, u_message = user()
+        if u_code == 331:  # "Укажите свой пароль"
+            p_code, p_message = passwd()
+            if p_code == 230:  # Удачная авторизация
+                print("Добро пожаловать, " + username)
+                return True
+            else:
+                print("Ошибка: " + p_message +
+                      " Код ответа: " + str(p_code) + ".")
+                return False
+        else:
+            print("Ошибка: " + u_message +
+                  " Код ответа: " + str(u_code) + ".")
+            return False
+
 
     @staticmethod
-    def cd(command_connection):
+    def cd(command_connection, folder=""):
         """
         CWD
-        :rtype : object
         """
-        pass
+        if folder == "":
+            folder = input("Пожалуйста, введите имя директории: ")
+        command_connection.send_cmd("CWD" + folder)
+        code, message = command_connection.get_reply()
+        print(code, message)
+
 
     @staticmethod
-    def logout(command_connection):
-        pass
-
-    @staticmethod
-    def pwd(command_connection):
-        pass
+    def quit(command_connection):
+        command_connection.send_cmd("QUIT")
 
     @staticmethod
     def noop(command_connection):
@@ -104,5 +135,16 @@ class Commands:
         Возвращает "OK!"
         """
         command_connection.send_cmd("NOOP")
+        code, message = command_connection.get_reply()
+        if code == 200:
+            print("Всё хорошо :)")
+            return True
+        else:
+            print("Ошибка: " + message + " Код ответа: " + str(code) + ".")
+            return False
+
+    @staticmethod
+    def pwd(command_connection):
+        command_connection.send_cmd("PWD")
         code, message = command_connection.get_reply()
         print(code, message)
